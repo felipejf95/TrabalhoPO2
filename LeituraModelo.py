70e3ee3e541a02d8e6b88c9865c731c1db5c0c24
felipejf95 <felipejf95@hotmail.com>
2023-07-11 19:52:21 -0300

    leitura do modelo, otimização e exportação p csv

--- LeituraModelo.py
+++ LeituraModelo.py
@@ -1,8 +1,26 @@
 import gurobipy as gp
 from gurobipy import GRB
+import pandas as pd
 
 modelo = gp.Model()
 
 modelo = gp.read('modeloTeste.lp')
 
 modelo.optimize()
+
+
+
+dados = []
+
+dados.append(["Função objetivo", modelo.objVal])
+
+for v in modelo.getVars():
+    dados.append([v.varName, v.x])
+
+
+df = pd.DataFrame(dados, columns=["Variável", "Valor"])
+
+df.to_csv("dados.csv", index=False)
+
+print("Valor da função objetivo:", modelo.objVal)
+print(df)
